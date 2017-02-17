a2ad8a5f83c5a8ba522c066f6ca45b3184063fa7
mikhailkovalenko <mikhail.kovalenko96@gmail.com>
2017-02-17 17:54:23 +0300

    New Project

---
+++ #1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+struct Stack {
+    private:
+        int value;
+        double *data;
+
+    public:
+        Stack(){
+            
+        }
+};
+
+int pop {
+    if (lengh == 0) {
+
+    }
+};
+
+int main() {
+    List *NewList = new List;
+
+    Show(NewList);
+    return 0;
+}
